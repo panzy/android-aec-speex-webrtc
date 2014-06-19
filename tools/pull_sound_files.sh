142d611034b25e7a3932e5ee51b2ccf43b0d5f93
panzy/work-win7 <panzhiyong84@gmail.com>
2014-06-19 15:07:47 +0800

    Fix: 脚本在 Windows sh.exe 中不能运行。

--- tools/pull_sound_files.sh
+++ tools/pull_sound_files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-adb pull sdcard/tmp/speaker.raw tmp/
-adb pull sdcard/tmp/far.raw tmp/
-adb pull sdcard/tmp/near.raw tmp/
-adb pull sdcard/tmp/send.raw tmp/
+adb pull sdcard/tmp/speaker.raw tmp
+adb pull sdcard/tmp/far.raw tmp
+adb pull sdcard/tmp/near.raw tmp
+adb pull sdcard/tmp/send.raw tmp
